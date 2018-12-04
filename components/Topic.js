639c2ade9a1b30d94c26afae7981995dac8f4970
hezuodong <hezuodong@daojia-inc.com>
2018-12-04 18:37:55 +0800

    mobile first

--- components/Topic.js
+++ components/Topic.js
@@ -1,15 +1,15 @@
 import Link from 'next/link';
 
-export default ({item}) => (
+export default ({item, index}) => (
     <article className="topic col-xl-3 mx-2 mt-4">
         <Link href="/">
-            <a>
+            <a className="position-relative height-full d-flex no-underline">
                 <div className="position-absolute top-0 right-0 bottom-0 left-0 overflow-hidden">
                     <img src={item.imgUrl} alt={item.title} className="width-fit mx-auto" />
                 </div>
                 <div className="z-index-100">
-                    <div>
-                        <h1>{item.title}</h1>
+                    <div className={`p-6 overflow-hidden ${index > 0 ? 'text-gray-white' : 'text-gray-dark'}`}>
+                        <h2 className="h2">{item.title}</h2>
                         <p className="f5 mb-0">{item.content}</p>
                     </div>
                     <div></div>
@@ -22,6 +22,30 @@ export default ({item}) => (
                 display: block;
                 position: relative;
             },
+            .h2 {
+                font-size: 24px;
+            }
+            .f5 {
+                font-size: 14px;
+            }
+            .text-gray-white {
+                color: white;
+            }
+            .text-gray-dark {
+                color: #24292e;
+            }
+            .position-relative: {
+                position: relative;
+            }
+            .height-full {
+                height: 100%;
+            }
+            .d-flex {
+                display: flex;
+            }
+            .no-underline {
+                text-decoration: none;
+            }
             .width-fit {
                 width: 100%;
             }
@@ -36,6 +60,9 @@ export default ({item}) => (
             }
             .mt-4 {
                 margin: 16px 0 0; 
+            }
+            .p-6 {
+                padding: 24px;
             }
             .position-absolute {
                 position: absolute;
@@ -58,7 +85,6 @@ export default ({item}) => (
             .z-index-100 {
                 z-index: 100;
             }
-            
         `}
         </style>
     </article>
--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,20 @@ import config from '../config';
 
 const Index = ({topics}) => (
     <Layout>
-        <div className="d-lg-flex container-xl">
-            {topics.map((topic, key) => ((
-                <Topic item={topic} key={key} />
-            )))}
+        <div className="px-4 bg-grey-dark">
+            <div className="d-lg-flex container-xl">
+                {topics.map((topic, key) => ((
+                    <Topic item={topic} key={key} index={key} />
+                )))}
+            </div>
         </div>
         <style jsx>{`
+            .px-4 {
+                padding: 0 16px;
+            }
+            .bg-grey-dark {
+                background-color: #24292e;
+            }
             .container-xl {
                 max-width: 1280px;
                 width: 100%;
